9f2084c2fa4667cee9822bdf72f8aa770faba937
Harsh112103087 <100709008+harshmarke@users.noreply.github.com>
2023-10-24 00:44:48 +0530

    Update and rename 24-October-2023 to 24-October-2023.cpp

--- 2023/24-October-2023.cpp
+++ 2023/24-October-2023.cpp
@@ -31,10 +31,10 @@ int solve(string s,int i,int j,vector<vector<int>> &dp){
     return dp[i][j] = mn;
 }
 
-** Time Complexity : O(n^3) **
-** Space Complexity : O(n^2) **
+// Time Complexity : O(n^3) 
+// Space Complexity : O(n^2)
 
-** Explanation :  ** 
+// Explanation :
 The isPalindrome function checks if a given substring is a palindrome. It uses a while loop to compare characters from the beginning and end of the substring, returning true if it's a palindrome and false if not.
 The solve function is a recursive function that finds the minimum number of partitions needed for a given substring. It employs memoization with a 2D DP array to avoid redundant calculations. The function iterates through all possible partitions and calculates the minimum number of partitions required for each substring. It calls the isPalindrome function, which takes O(j - i) time.
 The palindromicPartition function initializes a 2D DP array and calls the solve function to solve the problem for the entire input string. The result of the problem is stored in the DP array.
